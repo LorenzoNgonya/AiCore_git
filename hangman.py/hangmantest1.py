11008800ec07ec84eeef1ba5a260dd3c94ea12e2
LorenzoNgonya <lorenzongonya@gmail.com>
2022-09-26 06:05:43 +0100

    now able got get the random word

---
+++ hangman.py/hangmantest1.py
@@ -0,0 +1,7 @@
+import random
+from milestone_1 import word_list
+
+def get_word(word_list):
+    word = random.choice(word_list)
+    
+    
--- hangman.py/milestone_1.py
+++ hangman.py/milestone_1.py
@@ -1,9 +1,10 @@
 # my list of favourite fruits 
 
-word_list = [
-    "papayas",
-    "bananas",
-    "grapes",
-    "strawberries",
-    "avocado"]
+word_list = ["papayas","bananas","grapes","strawberries","avocado"]
 print (word_list)
+
+import random
+
+word = random.choice(word_list)
+
+print (word)
