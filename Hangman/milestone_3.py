f50a9cd42c9770a34b9da216193e28a46f515ffe
LorenzoNgonya <lorenzongonya@gmail.com>
2022-09-28 05:02:33 +0100

    will now be able to check it's a single letter

---
+++ Hangman/milestone_3.py
@@ -0,0 +1,27 @@
+# this will add user input
+
+word_list = ["papayas","bananas","grapes","strawberries", "avocado"]
+print (word_list)
+
+import random
+from string import ascii_lowercase
+
+## choose a random word 
+
+word = random.choice(word_list)
+print(word)
+
+### user input 
+
+guess = input("guess a letter")
+
+print(guess)
+
+### if statement that checks it's a single letter 
+
+if len(guess) ==1:
+    if guess in ascii_lowercase:
+        print("Good guess!")
+        
+else:
+    print("Oops! That is not a valid input")
