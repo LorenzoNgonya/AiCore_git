33d7e102cd8e3e13b446bfc7001b9bbf91f86b92
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-10-10 06:13:15 +0100

    now able to validate the guessed letter

---
+++ Hangman/milestone_2/milestone_2v1.py
@@ -0,0 +1,12 @@
+# code that will continuously ask the user for a letter and validate it.
+
+from string import ascii_lowercase
+
+
+while True:
+    guess = input ("guess a letter")
+    if len(guess) ==1:
+      if guess in ascii_lowercase:
+        break
+    else :
+        print ("Invalid letter. Please, enter a single alphabetical character.")
