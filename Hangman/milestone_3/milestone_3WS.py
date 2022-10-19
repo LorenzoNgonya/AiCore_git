23b0bcedb0be583ca31f2be2d6b4414e2c3f115e
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-10-19 03:03:25 +0100

    all code is working now

--- Hangman/milestone_3/milestone_3WS.py
+++ Hangman/milestone_3/milestone_3WS.py
@@ -33,24 +33,25 @@ class Hangman:
     
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = [f'',len(word)]
+        self.word_guessed = [f"_",len(word)]
         self.num_letters = word.__sizeof__
         self.num_lives = num_lives
         self.word_list = ["papayas","bananas","grapes","strawberries", "avocado"]
         self.list_of_guesses = [len(guess)]
 
-        def check_guess_method(self):
-            if len(guess) ==1 and guess in ascii_lowercase:
-                print ("Good guess! {} is in the word.".format(guess))
+    def check_guess_method(self):
+        if len(guess) ==1 and guess in ascii_lowercase:
+            print ("Good guess! {} is in the word.".format(guess))
 
-        def ask_for_input(self):
-            while True:
-                guess = input ("guess a letter")
-                if guess not in ascii_lowercase:
-                    print ("Invalid letter. Please, enter a single alphabetical character.")
-                elif guess in {self.list_of_guesses}:
-                        print ("You already tried that letter!")
-                else :
-                    check_guess_method(guess)
-                    {self.list_of_guesses}.append(guess)
+    def ask_for_input (self):
+        while True:
+            guess = input ("guess a letter")
+            if guess not in ascii_lowercase:
+                print ("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in {self.list_of_guesses}:
+                    print ("You already tried that letter!")
+            else :
+                self.check_guess_method(guess)
+                {self.list_of_guesses}.append(guess)
 
+Hangman.ask_for_input()
