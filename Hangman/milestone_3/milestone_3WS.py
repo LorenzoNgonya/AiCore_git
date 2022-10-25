805d4d46108b9f4b0ddbb28c17e4695435a8a071
Lorenzo <114039201+LorenzoNgonya@users.noreply.github.com>
2022-10-25 08:37:43 +0100

    break

--- Hangman/milestone_3/milestone_3WS.py
+++ Hangman/milestone_3/milestone_3WS.py
@@ -2,11 +2,15 @@
 word_list = ["papayas","bananas","grapes","strawberries", "avocado"]
 print (word_list)
 
-from lib2to3.pytree import LeafPattern
 from opcode import hasjabs
 import random
 from re import S, X
 from string import ascii_lowercase
+
+
+
+
+
 
 
 
@@ -17,19 +21,19 @@ print(word)
 
 # code that will continuously ask the user for a letter and validate it.
 
-while True:
-    guess = input ("guess a letter")
-    if len(guess) ==1 and guess in ascii_lowercase: # checks that code is a single alphabetical character 
-        break
-    else :
-        print ("Invalid letter. Please, enter a single alphabetical character.")
+# while True:
+#     guess = input ("guess a letter ")
+#     if len(guess) ==1 and guess in ascii_lowercase: # checks that code is a single alphabetical character 
+#         break
+#     else :
+#         print ("Invalid letter. Please, enter a single alphabetical character.")
         
 # Checks if the letter guessed by the user is in the secret word that was randomly chosen by the computer. 
 
-if guess in word:  # if statement that checks if the guess is in the word.
-    print ("Good guess! {} is in the word.".format(guess))
-else:
-       print ("Sorry, {} is not in the word. Try again.".format(guess)) # This code will run if the guess is not in the word.
+# if guess in word:  # if statement that checks if the guess is in the word.
+#     print ("Good guess! {} is in the word.".format(guess))
+# else:
+#        print ("Sorry, {} is not in the word. Try again.".format(guess)) # This code will run if the guess is not in the word.
 
 # code for the hangman classs to develop rest of the game 
 
@@ -37,21 +41,23 @@ class Hangman:
 #      __init__ method to initialise the attributes of the class. Word_list and num_lives as parameters.
     def __init__(self, word_list, num_lives=5):
 
-# Initialise the following attributes:
+# Initialises the following attributes:
         self.word = random.choice(word_list)
-        self.word_guessed = [f"_",len(word)]
+        self.word_guessed = ['_ '*len(self.word)]
         self.num_letters = word.__sizeof__
         self.num_lives = num_lives
         self.word_list = ["papayas","bananas","grapes","strawberries", "avocado"]
         self.list_of_guesses = []
 
 # check_guess method that will ask the user to guess a letter and another method that will check if the guess is in the word.
-    def check_guess_method(self):
+    def check_guess_method(self, guess):
         if len(guess) ==1 and guess in ascii_lowercase:
             print ("Good guess! {} is in the word.".format(guess))
-        for i in range (len(self.word)):
-            if guess == self.word[i]:
-                print (i.join(self.word_guessed))
+            for i in range (len(self.word)):
+                if guess == self.word[i]:
+                    self.word_guessed = self.word_guessed + [i]
+                    print(self.word_guessed)
+                #self.word_guessed = ''.join(self.word_guessed)
         else:
             self.num_lives = self.num_lives - 1
             print ("Sorry, {} is not in the word.".format(guess))
@@ -61,16 +67,20 @@ class Hangman:
     
     def ask_for_input (self):
         while True:
-            guess = input ("guess a letter")
+            guess = input ("guess a letter ")
             if guess not in ascii_lowercase:
                 print ("Invalid letter. Please, enter a single alphabetical character.")
-            elif guess in {self.list_of_guesses}:
+            elif guess in self.list_of_guesses:
                     print ("You already tried that letter!")
             else :
                 self.check_guess_method(guess)
-                {self.list_of_guesses}.append(guess)
+                self.list_of_guesses.append(guess)
 
 
-Hangman.ask_for_input(word_list)
+#Hangman.ask_for_input(word_list)
+
+game = Hangman(word_list)
+game.ask_for_input()
+
 
 
